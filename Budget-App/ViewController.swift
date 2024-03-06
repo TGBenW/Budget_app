7c028e2d6ad2fae3cbf8b37dd09c9153f979e3fe
Andrey Manuilov <dukariga@gmail.com>
2024-03-06 17:44:03 +0200

    Initial Commit

---
+++ Budget-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget-App
+//
+//  Created by Andrey Manuilov on 06/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
